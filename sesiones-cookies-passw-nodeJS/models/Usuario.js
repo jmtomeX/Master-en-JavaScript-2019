6473a2bdd97c600b2d3cec3ae7d8ac358207a3b3
jmtomeX <jmtomeX@gmail.com>
2019-12-30 20:52:41 +0100

    aplicación de login con Nodejs terminada

--- sesiones-cookies-passw-nodeJS/models/Usuario.js
+++ sesiones-cookies-passw-nodeJS/models/Usuario.js
@@ -1,6 +1,7 @@
-
-const bcrypt = require("bcrypt-nodejs");;
-const mongoose = require('mongoose');
+const bcrypt = require("bcrypt-nodejs");
+const mongoose = require("mongoose");
+// PARA EL ERROR -> (node:2340) DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
+mongoose.set('useCreateIndex', true);
 const Schema = mongoose.Schema;
 
 const usuarioSchema = new Schema(
@@ -27,27 +28,40 @@ const usuarioSchema = new Schema(
 );
 // método que mongoose ejecuta antes de guardar el objeto
 // usa el módulo de node bcrypt-nodejs
-usuarioSchema.pre('save', function(next){
-    const usuario = this;
-    if(!usuario.isModified('password')){
-        return next();
+usuarioSchema.pre("save", function(next) {
+  const usuario = this;
+  if (!usuario.isModified("password")) {
+    return next();
+  }
+  // encriptar la contraseña, que itere 10 veces
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      next(err);
     }
-    // encriptar la contraseña, que itere 10 veces
-    bcrypt.gentSalt(10, (err, salt) => {
-        if(err){
-            next(err);
-        }
-        // creamos un hash de la contraseña
-        bcrypt.hash(usuario.password, salt, null, (err, hash) => {
-            if(err){
-                next(err);
-            }
-            // sustituimos  la contraseña del usuario  por la que  acabamos de obtener 
-            usuario.password = hash;
-            // llamamos a next sin error, para que la información pueda ser almacenada
-            next();
-        });
-    })
+    // creamos un hash de la contraseña
+    bcrypt.hash(usuario.password, salt, null, (err, hash) => {
+      if (err) {
+        next(err);
+      }
+      // sustituimos  la contraseña del usuario  por la que  acabamos de obtener del hash
+      usuario.password = hash;
+      // llamamos a next sin error, para que la información pueda ser almacenada
+      next();
+    });
+  });
+});
 
-})
-
+// comparar contraseña
+usuarioSchema.methods.comparePassword = function(password, cb) {
+  // (passw que está haciendo login, passw que está almacenado, comparación )
+  bcrypt.compare(password, this.password, (err, sonIguales) => {
+    // en caso de error se le llama alcallback con el error
+    if (err) {
+      return cb(err);
+    }
+    // si son iguales le mandamos  sin error y el boolean 
+    cb(null, sonIguales);
+  });
+}
+// crea el modelo IUsuario y el esquema de cual es la información del usuario
+module.exports = mongoose.model('Usuario', usuarioSchema);
--- sesiones-cookies-passw-nodeJS/server.js
+++ sesiones-cookies-passw-nodeJS/server.js
@@ -6,6 +6,13 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const mongoose = require('mongoose');
 
+//npm i --save body-parser
+const bodyParser = require('body-parser');
+const passport = require("passport");
+// configuración passport
+const passportConfig = require('./config/passport');
+
+
 
 // conexión a MongoDB, persistir las sesiones
 const MONGO_URL = 'mongodb://127.0.0.1:27017/auth';
@@ -15,15 +22,39 @@ const app = express();
 
 // configurar mongoose
 // configurar las promesas que va a utilizar, las mismas que las de nodejs
-mongoose.Promise = glibal.Promise;
+mongoose.Promise = global.Promise;
 // que se conecte a mongo
-mongoose.connect(MONGO_URL);
+mongoose.connect(MONGO_URL,
+    { useNewUrlParser: true,
+        useUnifiedTopology: true 
+    
+    });
 // que hace cuando se conecte
 mongoose.connection.on('error', (err)=> {
     throw err;
     // paramos el proceso si hay errores
     process.exit(1);
+});
+
+// Prueba de usuario
+/*
+
+const Usuario = require('./models/Usuario');
+const user1 = new Usuario ({
+    email: 'iremti2@gmail.com',
+    password: '123456',
+    name: 'Jose Mari'
 })
+// el método save devuelve una promesa
+user1.save()
+.then( () => {
+    console.log('Usuario guardado')
+})
+.catch((e) => {
+    console.log(e);
+
+})
+*/
 // que utilice el middleware session
 app.use(session({
     //encrypta los id
@@ -37,14 +68,43 @@ app.use(session({
         url: MONGO_URL,
         autoReconnect:true
     })
-}))
+}));
+
+// configuración passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//codificación a json
+app.use(bodyParser.json());
+// la información que llega del formulario viene codificada en el cuerpo de la llamda y url enconde
+//la transforma json para poder leerla de una forma má sencilla
+// En desuso -> ERROR -> body-parser deprecated undefined extended: provide extended option node_modules\express\lib\router\layer.js:95:5
+//app.use(bodyParser.urlencoded);
+app.use (bodyParser.urlencoded ({extended: true}));
 
 // declaramos la primera ruta de la aplicación (home)
-app.get('/', (req, res) => {
-    // en el caso de que la cuenta  exista le añadimos 1, si no la inicializamos en 1, 
-    req.session.cuenta = req.session.cuenta ? req.session.cuenta + 1: 1;
-    res.send(`Hola! Has visto esta página : ${req.session.cuenta}`)
+// app.get('/', (req, res) => {
+//     // en el caso de que la cuenta  exista le añadimos 1, si no la inicializamos en 1, 
+//     req.session.cuenta = req.session.cuenta ? req.session.cuenta + 1: 1;
+//     res.send(`Hola! Has visto esta página : ${req.session.cuenta}`)
+// });
+
+// importamos al controlador usuario
+const controladorUsuario = require('./controllers/usuario');
+
+// crear las rutas
+// registrar un usuario
+app.post('/signup', controladorUsuario.postSignup);
+// login
+app.post('/login', controladorUsuario.postLogin);
+// 
+app.get('/logout', passportConfig.estaAutenticado, controladorUsuario.logout);
+// retorna la info del usuario
+app.get('/usuarioInfo', passportConfig.estaAutenticado, (req, res)=> {
+    // user lo agrega de esta forma
+    res.json(req.user);
 });
+
 // que escuche en el puerto 3000
 app.listen(3000, () => {
     console.log('Escuchando en el puerto 3000');
