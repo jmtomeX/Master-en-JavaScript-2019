cdd9f67860424f7b961e1275e3dd429da4c74d0e
jmtomeX <jmtomeX@gmail.com>
2020-01-06 17:36:24 +0100

    Borrado nodejs

--- NodeJS/Curso NodeJs/01_helloWorld/helloWorld.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var controlador = function(req, res) {
-    console.log("Recibimos una nueva petición al servidor");
-    res.end("Hola mundo");
-}
-
-var servidor = http.createServer(controlador);
-
-servidor.listen('3000');
